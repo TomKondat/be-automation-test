7c956be680766cceccce6a32ea2bcac619dcf028
Tom Kondat <tomkondat5@gmail.com>
2024-05-27 20:22:10 +0300

    first commit

---
+++ src/main/java/com/tomkondat5/app/App.java
@@ -0,0 +1,13 @@
+package com.tomkondat5.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
